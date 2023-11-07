54d29986a0b0d86cb4f2401083099279f6c0f136
Artur Rodrigues <artur190503@outlook.com>
2023-11-07 16:29:29 -0300

    Formativa 2 - Questão B

--- Formativas/Formativa 2/B.c
+++ Formativas/Formativa 2/B.c
@@ -11,23 +11,6 @@ int hash(int ch, int M) {
     return ch % M;
 } 
 
-int aumentaTamanho(int M) {
-    int novoTamanho = 2 * M + 1;
-    while (1) {
-        int primo = 1;
-        for (int i = 2; i * i <= novoTamanho; ++i) {
-            if (novoTamanho % i == 0) {
-                primo = 0;
-                break;
-            }
-        }
-        if (primo) {
-            return novoTamanho;
-        }
-        novoTamanho += 2;
-    }
-}
-
 void inicializaTabela(TH *h, int tamanho) {
     h->tb = (int *)malloc(tamanho * sizeof(int));
     h->M = tamanho;
@@ -37,36 +20,16 @@ void inicializaTabela(TH *h, int tamanho) {
     }
 }
 
-void redimensionaTabela(TH *h) {
-    int novoTamanho = aumentaTamanho(h->M);
-    int *novaTabela = (int *)malloc(novoTamanho * sizeof(int));
-    for (int i = 0; i < novoTamanho; ++i) {
-        novaTabela[i] = -1; // Inicializa todas as posições com -1, indicando vazio.
-    }
-
-    for (int i = 0; i < h->M; ++i) {
-        if (h->tb[i] != -1) {
-            int posicao = hash(h->tb[i], novoTamanho);
-            while (novaTabela[posicao] != -1) {
-                posicao = hash((posicao + 1), novoTamanho);
-            }
-            novaTabela[posicao] = h->tb[i];
-        }
-    }
-
-    free(h->tb);
-    h->tb = novaTabela;
-    h->M = novoTamanho;
-}
-
 void THinsere(TH *h, int ch) {
-    if (h->N > h->M / 2) {
-        redimensionaTabela(h);
-    }
-
     int posicao = hash(ch, h->M);
+    int inicial = posicao;
     while (h->tb[posicao] != -1) {
         posicao = hash((posicao + 1), h->M); // Sondagem linear para encontrar a próxima posição vazia.
+        if (posicao == inicial) {
+            // A tabela está completamente cheia, você pode decidir como lidar com esse caso.
+            // Por exemplo, pode redimensionar a tabela aqui, mas essa lógica não está incluída neste código.
+            return;
+        }
     }
 
     h->tb[posicao] = ch;
@@ -75,6 +38,7 @@ void THinsere(TH *h, int ch) {
 
 int THremove(TH *h, int ch) {
     int posicao = hash(ch, h->M);
+    int inicial = posicao;
     while (h->tb[posicao] != -1) {
         if (h->tb[posicao] == ch) {
             h->tb[posicao] = -1; // Marca a posição como vazia.
@@ -93,6 +57,9 @@ int THremove(TH *h, int ch) {
             return 0; // Chave encontrada e removida.
         }
         posicao = hash((posicao + 1), h->M); // Sondagem linear para procurar a chave.
+        if (posicao == inicial) {
+            break;
+        }
     }
 
     return -1; // Chave não encontrada na tabela.
@@ -100,34 +67,52 @@ int THremove(TH *h, int ch) {
 
 int THbusca(TH *h, int ch) {
     int posicao = hash(ch, h->M);
+    int inicial = posicao;
     while (h->tb[posicao] != -1) {
         if (h->tb[posicao] == ch) {
             return 1; // Chave encontrada na tabela.
         }
         posicao = hash((posicao + 1), h->M); // Sondagem linear para procurar a chave.
+        if (posicao == inicial) {
+            break;
+        }
     }
     return 0; // Chave não encontrada na tabela.
 }
 
+void liberarTabela(TH *h) {
+    free(h->tb);
+    h->tb = NULL;
+    h->M = 0;
+    h->N = 0;
+}
+
 int main() {
-    // Exemplo de uso das funções:
-
     TH tabela;
-    inicializaTabela(&tabela, 10); // Inicializa a tabela com tamanho 10.
+    inicializaTabela(&tabela, 10); // Tamanho inicial da tabela é 10
 
-    THinsere(&tabela, 5);
-    THinsere(&tabela, 15);
-    THinsere(&tabela, 25);
-    THinsere(&tabela, 35);
+    // Exemplo de uso
+    THinsere(&tabela, 42);
+    THinsere(&tabela, 18);
+    THinsere(&tabela, 56);
+    
+    int chaveBusca = 18;
+    if (THbusca(&tabela, chaveBusca)) {
+        printf("Chave %d encontrada na tabela.\n", chaveBusca);
+    } else {
+        printf("Chave %d não encontrada na tabela.\n", chaveBusca);
+    }
 
-    printf("Busca 15: %d\n", THbusca(&tabela, 15)); // Deve imprimir 1 (verdadeiro).
-    printf("Busca 20: %d\n", THbusca(&tabela, 20)); // Deve imprimir 0 (falso).
+    THremove(&tabela, 18);
+    
+    chaveBusca = 18;
+    if (THbusca(&tabela, chaveBusca)) {
+        printf("Chave %d encontrada na tabela.\n", chaveBusca);
+    } else {
+        printf("Chave %d não encontrada na tabela.\n", chaveBusca);
+    }
 
-    THremove(&tabela, 15);
-
-    printf("Busca 15 após remoção: %d\n", THbusca(&tabela, 15)); // Deve imprimir 0 (falso).
-
-    free(tabela.tb); // Libera a memória alocada para a tabela.
+    liberarTabela(&tabela);
 
     return 0;
 }
